d455d03e547e70b0cbe2fb47d1997ddfae68151d
Ultra-tech-code <hizick27@gmail.com>
2023-10-07 09:47:41 +0100

    update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -13,10 +13,15 @@ async function main() {
    
    // call the setGreetings function
    const tx = await TestContractInstance.setGreetings("World");
+    await tx.wait();
+   console.log(`Transaction hash: ${tx.hash}`);
+  
 
   // call the getGreetings function
   const greetings = await TestContractInstance.getGreetings();
   console.log(`Greetings: ${greetings}`);
+
+  // Deployed Address = 0xCA474E7054Ef89eEb20B2d03411bDF2C340fF236
 
 }
 
